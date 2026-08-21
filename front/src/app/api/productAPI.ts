36366df2b7f3dea2a1366ee562950afa8c5b63ea
NKGarzonGrajales <karycyber22@hotmail.com>
2026-08-20 21:19:56 -0500

    fix: update API port to 3004

--- front/src/app/api/productAPI.ts
+++ front/src/app/api/productAPI.ts
@@ -1,6 +1,6 @@
 import {IProduct} from "@/interfaces/types";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001";
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3004";
 
 export async function getProducts(): Promise<IProduct[]> {
     try {
